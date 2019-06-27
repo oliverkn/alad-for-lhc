172bf35dad59ca3b3e16a77cf51f0d0aa78790e4
Oliver Knapp <oliver.knapp@outlook.com>
2019-06-27 17:40:24 +0200

    added euler support for data_builder

--- data/dataset_builder.py
+++ data/dataset_builder.py
@@ -1,11 +1,18 @@
 from data.raw_loader import *
 
+mode = 'pc'  # 'euler'
+
 # path and file information
-path = '/home/oliverkn/pro/data_raw/'
+if mode == 'pc':
+    path = '/home/oliverkn/pro/data_raw/'
+    target_path = '/home/oliverkn/pro/data/4_4'
+
+if mode == 'euler':
+    path = '/cluster/home/knappo/data_raw/'
+    target_path = '/cluster/home/knappo/data_raw/4_4'
+
 sm_file_names = ['Wlnu', 'Zll', 'ttbar', 'qcd']
 bsm_file_names = ['Ato4l', 'leptoquark', 'hToTauTau', 'hChToTauNu']
-
-target_path = '/home/oliverkn/pro/data/4_4'
 
 print('load data')
 sm_files = [path + s + '_sample.npy' for s in sm_file_names]
