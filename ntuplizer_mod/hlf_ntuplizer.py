62ccaa60bc040a72cddc6fc8322de2e6c4a3585d
Oliver Knapp <oliver.knapp@outlook.com>
2019-08-20 21:16:09 +0200

    added some more quantities for hlf ntuplizer

--- ntuplizer_mod/hlf_ntuplizer.py
+++ ntuplizer_mod/hlf_ntuplizer.py
@@ -26,7 +26,7 @@ class MuonSelector(AbstractSelectorModule):
         return 'muon pt>23 and iso<0.45'
 
 
-class HT_module(AbstractQuantityModule):
+class JetModule(AbstractQuantityModule):
     """
        take all double_ak5PFJets_sigma_RECO elements with pT>30 and
        compute the scalar sum of their pTs
@@ -35,31 +35,89 @@ class HT_module(AbstractQuantityModule):
     def compute(self, values):
         pt = values['recoPFJets_ak5PFJets__RECO.obj.pt_']
         HT = np.zeros(pt.shape[0])
+        njets = np.zeros(pt.shape[0])
         for i in range(pt.shape[0]):
-            pt_f = np.where(pt[i] > 30, pt[i], 0)
-            HT[i] = np.sum(pt_f)
+            mask = pt[i] > 30
 
-        return HT.reshape((-1, 1))
+            HT[i] = np.sum(pt[i][mask])
+            njets[i] = np.sum(mask)
+
+        # TODO: nbjets, massJet
+
+        return np.stack([HT, njets], axis=1)
 
     def get_keys(self):
         return ['recoPFJets_ak5PFJets__RECO.obj.pt_']
 
-    def get_size(self): return 1
+    def get_size(self): return 2
 
-    def get_names(self): return ['HT']
+    def get_names(self): return ['HT', 'nJets']
+
+
+class LeptonModule(AbstractQuantityModule):
+    """
+        You need to select ONE lepton here. Among all the leptons that pass
+        your selection (we need to clarify with Olmo what we are doing here.
+        If we only run on the SingleMuon dataset, this lepton is always a muon)
+        you need to select the one with highest pT.
+    """
+
+    KEY_PT = 'recoMuons_muons__RECO.obj.pt_'
+    KEY_ETA = 'recoMuons_muons__RECO.obj.eta_'
+
+    def compute(self, values):
+        mu_pt = values[self.KEY_PT]
+        mu_eta = values[self.KEY_ETA]
+
+        lep_pt = np.zeros(mu_pt.shape[0])
+        lep_eta = np.zeros(mu_pt.shape[0])
+
+        for i in range(mu_pt.shape[0]):
+            j_max = np.argmax(mu_pt[i])
+
+            lep_pt[i] = mu_pt[i][j_max]
+            lep_eta[i] = mu_eta[i][j_max]
+
+        return np.stack([lep_pt, lep_eta], axis=1)
+
+    def get_keys(self):
+        return [self.KEY_PT, self.KEY_ETA]
+
+    def get_size(self): return 2
+
+    def get_names(self): return ['lep_pt', 'let_eta']
+
+
+class METModule(AbstractQuantityModule):
+    KEY_PT = 'recoPFMETs_pfMet__RECO.obj.pt_'
+    KEY_PHI = 'recoPFMETs_pfMet__RECO.obj.phi_'
+
+    def compute(self, values):
+        met = values[self.KEY_PT]
+        phi = values[self.KEY_PHI]
+
+        return np.hstack([met, phi])
+
+    def get_keys(self):
+        return [self.KEY_PT, self.KEY_PHI]
+
+    def get_size(self): return 2
+
+    def get_names(self): return ['MET', 'phiMET']
 
 
 ntuplizer = Ntuplizer()
-ntuplizer.register_quantity_module(HT_module())
 ntuplizer.register_selector(MuonSelector())
+ntuplizer.register_quantity_module(JetModule())
+ntuplizer.register_quantity_module(METModule())
+ntuplizer.register_quantity_module(LeptonModule())
 
 result, names = ntuplizer.convert('/home/oliverkn/pro/real_data_test/test.root')
 
 print(result.shape)
-
-# print(names)
+print(names)
 #
-# hdf5_file = h5py.File('/home/oliverkn/pro/real_data_test/test.hdf5', "w")
-# hdf5_file.create_dataset('data', data=result, compression='gzip')
+hdf5_file = h5py.File('/home/oliverkn/pro/real_data_test/test.hdf5', "w")
+hdf5_file.create_dataset('data', data=result, compression='gzip')
 # # hdf5_file.create_dataset('names', data=names, dtype=h5py.special_dtype(vlen=str), compression='gzip')
-# hdf5_file.close()
+hdf5_file.close()
