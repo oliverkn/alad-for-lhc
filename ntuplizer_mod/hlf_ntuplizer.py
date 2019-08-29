b0c4e87bfd92862472d1f66a02ca46eb5fc09ab4
Oliver Knapp <oliver.knapp@outlook.com>
2019-08-29 10:15:29 +0200

    ntuplizer: adap for lxplus

--- ntuplizer_mod/hlf_ntuplizer.py
+++ ntuplizer_mod/hlf_ntuplizer.py
@@ -320,24 +320,24 @@ class ParticleCountModule(AbstractQuantityModule):
         return ['n_' + self.term_suffix]
 
 
-ntuplizer = Ntuplizer()
-ntuplizer.register_selector(MuonSelector())
-ntuplizer.register_quantity_module(JetModule())
-ntuplizer.register_quantity_module(BTagModule())
-ntuplizer.register_quantity_module(MaxLeptonModule())
-ntuplizer.register_quantity_module(MuonsModule())
-ntuplizer.register_quantity_module(LeptonModule('ele', 'recoGsfElectrons_gsfElectrons__RECO.obj'))
-ntuplizer.register_quantity_module(ParticleCountModule('neu', 130))
-ntuplizer.register_quantity_module(ParticleCountModule('ch', 211))
-ntuplizer.register_quantity_module(ParticleCountModule('photon', 22))
-
-result, names = ntuplizer.convert('/home/oliverkn/pro/real_data_test/test.root')
-
-print(result.shape)
-print(names)
+# ntuplizer = Ntuplizer()
+# ntuplizer.register_selector(MuonSelector())
+# ntuplizer.register_quantity_module(JetModule())
+# ntuplizer.register_quantity_module(BTagModule())
+# ntuplizer.register_quantity_module(MaxLeptonModule())
+# ntuplizer.register_quantity_module(MuonsModule())
+# ntuplizer.register_quantity_module(LeptonModule('ele', 'recoGsfElectrons_gsfElectrons__RECO.obj'))
+# ntuplizer.register_quantity_module(ParticleCountModule('neu', 130))
+# ntuplizer.register_quantity_module(ParticleCountModule('ch', 211))
+# ntuplizer.register_quantity_module(ParticleCountModule('photon', 22))
 #
-hdf5_file = h5py.File('/home/oliverkn/pro/real_data_test/test.hdf5', "w")
-hdf5_file.create_dataset('data', data=result, compression='gzip')
-hdf5_file.close()
-
-pickle.dump(names, open('/home/oliverkn/pro/real_data_test/test.pkl', 'wb'))
+# result, names = ntuplizer.convert('/home/oliverkn/pro/real_data_test/test.root')
+#
+# print(result.shape)
+# print(names)
+# #
+# hdf5_file = h5py.File('/home/oliverkn/pro/real_data_test/test.hdf5', "w")
+# hdf5_file.create_dataset('data', data=result, compression='gzip')
+# hdf5_file.close()
+#
+# pickle.dump(names, open('/home/oliverkn/pro/real_data_test/test.pkl', 'wb'))
